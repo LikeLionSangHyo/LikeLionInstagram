85e2b529057c0fde4b4ae2f4c1a8f43a8f897a24
LikeLionSangHyo <gilsport1@likelion.org>
2020-04-25 12:39:02 +0900

    Push heart2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const heart = document.querySelector('.heart_btn');
-
+const heart2 = document.querySelector("#main_container > div > div:nth-child(2) > article > div.bottom_icons > div.left_icons > div.heart_btn");
  heart.onclick = () => { 
      
     if((document.querySelector("#main_container > div > div > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className =="sprite_heart_icon_outline on"))
     document.querySelector("#main_container > div > div > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className ="sprite_heart_icon_outline";
     else
     document.querySelector("#main_container > div > div > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className ="sprite_heart_icon_outline on";
-     }
- 
+
+
+   } 
+   heart2.onclick= () => {
+           
+   if((document.querySelector("#main_container > div > div:nth-child(2) > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className == "sprite_heart_icon_outline on"))
+   document.querySelector("#main_container > div > div:nth-child(2) > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className ="sprite_heart_icon_outline";
+   else
+   document.querySelector("#main_container > div > div:nth-child(2) > article > div.bottom_icons > div.left_icons > div.heart_btn > div").className ="sprite_heart_icon_outline on";
+   }
